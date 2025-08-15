c7f34e82616c95bc60b60d39cf6c650d3d677de3
Vonn <vcserrano12@gmail.com>
2025-08-16 01:47:01 +0800

    ENV

--- hris-frontend/src/Login.js
+++ hris-frontend/src/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import logo from './assets/logo.png';
+import logo from './assets/logo2.png';
 
 const API_URL = process.env.REACT_APP_API_URL;
 
